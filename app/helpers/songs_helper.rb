a63d887505423fc169d4c27364e38896681d2ad0
zoebisch <zoebisch@gmail.com>
2017-09-21 10:37:57 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,8 @@
 module SongsHelper
 
   def artist_select(song, artist)
+    #test was really unclear where this should fit in.  Sincd there is no further test other than it looks for two variables,
+    #I am not sure where to go with this?
     binding.pry
   end
 
